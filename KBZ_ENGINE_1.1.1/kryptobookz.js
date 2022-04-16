aa5067e95bfc41a69edab890a317091d13bfec82
NetCinematics <alpinefalcon@protonmail.com>
2022-04-16 12:16:31 -0700

    remix 2

--- KBZ_ENGINE_1.1.1/kryptobookz.js
+++ KBZ_ENGINE_1.1.1/kryptobookz.js
@@ -487,7 +487,7 @@ function loadGalleryView(){ //
 function initSonics(){
     // sonics.spaceWind3= new BABYLON.Sound("spaceWind3","./copyrightnetcinematics/sonicz/nxSpaceWind3.mp3", nx.scene, null, { loop: false, autoplay: false, volume:0.44 });
     // sonics.track1 = new Audio("./copyrightNetCinematics/SONICS/track1.mp3");
-    sonics.track1 = new Audio("./copyrightNetCinematics/SONICS/SYNTHFUZZ_spazeFalcon_4443_warnpeace3.mp3");
+    sonics.track1 = new Audio("./copyrightNetCinematics/SONICS/SYNTHFUZZ_4443_3.mp3");
     sonics.track2 = new Audio("./copyrightNetCinematics/SONICS/track2.mp3");
 }
 
